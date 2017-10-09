60e165dada8d0eaab1f406466f6a618c04c0c16c
System Administrator <root@MacBook-Pro-de-everisIT.local>
2017-10-09 13:10:11 +0200

    Initial commit

---
+++ src/main/java/PoC/testing_ic/App.java
@@ -0,0 +1,13 @@
+package PoC.testing_ic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World, GBB!" );
+    }
+}
