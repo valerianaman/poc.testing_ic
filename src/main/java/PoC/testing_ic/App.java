d37a1bdbb7e673f6a62d7d5ceb7ce26a027fda3a
System Administrator <root@MacBook-Pro-de-everisIT.local>
2017-10-10 10:39:16 +0200

    Provoke error

--- src/main/java/PoC/testing_ic/App.java
+++ src/main/java/PoC/testing_ic/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+	int iVble;
         System.out.println( "Hello World, GBB! You're testing poll SCM" );
 /* this is my long development, so here I'm defining things */
     }
